bb01d63292a7ef140c7540bb285e8808d4fe4d8c
Raghu Betina <raghu@firstdraft.com>
2017-01-06 19:53:53 +0000

    Direct association between dishes and meals

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,9 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :meals,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,8 @@
 class Meal < ApplicationRecord
   # Direct associations
 
+  belongs_to :dish
+
   # Indirect associations
 
   # Validations
