ebfea5559d5d6f76141d476df4b447601854e5f1
Raghu Betina <raghu@firstdraft.com>
2017-01-06 19:53:38 +0000

    Generate dishes resource with starter:resource

---
+++ db/migrate/20170106195338_create_dishes.rb
@@ -0,0 +1,12 @@
+class CreateDishes < ActiveRecord::Migration
+  def change
+    create_table :dishes do |t|
+      t.integer :cuisine_id
+      t.integer :meal_id
+      t.string :name
+
+      t.timestamps
+
+    end
+  end
+end
